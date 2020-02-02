01383ab9456174e6889cbb7677c17496c2a0bb51
Tofu <athenavladimir@gmail.com>
2020-02-03 00:01:53 +0700

    Initial Commit

---
+++ InstargramSecond/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstargramSecond
+//
+//  Created by Apple on 2/3/20.
+//  Copyright © 2020 Tofu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
